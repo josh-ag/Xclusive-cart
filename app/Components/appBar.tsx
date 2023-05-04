d55912fc57d52b310cf80151d4e3e6528039cead
joshag <tech.gems.biz@gmail.com>
2023-05-04 22:27:05 +0100

    important update

--- app/Components/appBar.tsx
+++ app/Components/appBar.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
 
+import {
+  IoMenuOutline,
+  IoCartOutline,
+  IoHeartOutline,
+  IoSearchOutline,
+} from "react-icons/io5";
+
 const navItems = [
   { title: "Home", path: "/" },
   { title: "Contact", path: "/contact" },
@@ -33,97 +40,26 @@ export const AppbarComponent = () => {
           <div className="relative hidden md:flex items-center">
             <input
               type="search"
-              className="form-input rounded-md pr-10 border-0 bg-slate-100 focus:border-slate-500 focus:ring-slate-400"
+              className="form-input rounded-md pr-10 border-0 bg-slate-100 focus:border-slate-500 w-60 focus:ring-slate-400"
               placeholder="What are you looking for?"
             />
             <button className="absolute right-5">
-              <svg
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M17 17L13.2223 13.2156M15.3158 8.15789C15.3158 10.0563 14.5617 11.8769 13.2193 13.2193C11.8769 14.5617 10.0563 15.3158 8.15789 15.3158C6.2595 15.3158 4.43886 14.5617 3.0965 13.2193C1.75413 11.8769 1 10.0563 1 8.15789C1 6.2595 1.75413 4.43886 3.0965 3.0965C4.43886 1.75413 6.2595 1 8.15789 1C10.0563 1 11.8769 1.75413 13.2193 3.0965C14.5617 4.43886 15.3158 6.2595 15.3158 8.15789V8.15789Z"
-                  stroke="black"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                />
-              </svg>
+              <IoSearchOutline size={24} className="text-gray-400" />
             </button>
           </div>
           <Link href="/search" className="md:hidden">
-            <svg
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M17 17L13.2223 13.2156M15.3158 8.15789C15.3158 10.0563 14.5617 11.8769 13.2193 13.2193C11.8769 14.5617 10.0563 15.3158 8.15789 15.3158C6.2595 15.3158 4.43886 14.5617 3.0965 13.2193C1.75413 11.8769 1 10.0563 1 8.15789C1 6.2595 1.75413 4.43886 3.0965 3.0965C4.43886 1.75413 6.2595 1 8.15789 1C10.0563 1 11.8769 1.75413 13.2193 3.0965C14.5617 4.43886 15.3158 6.2595 15.3158 8.15789V8.15789Z"
-                stroke="black"
-                stroke-width="1.5"
-                stroke-linecap="round"
-              />
-            </svg>
+            <IoSearchOutline size={24} className="text-gray-600" />
           </Link>
           <Link href={"/"}>
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M8 5C5.7912 5 4 6.73964 4 8.88594C4 10.6185 4.7 14.7305 11.5904 18.8873C11.7138 18.961 11.8555 19 12 19C12.1445 19 12.2862 18.961 12.4096 18.8873C19.3 14.7305 20 10.6185 20 8.88594C20 6.73964 18.2088 5 16 5C13.7912 5 12 7.35511 12 7.35511C12 7.35511 10.2088 5 8 5Z"
-                stroke="black"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <IoHeartOutline size={24} className="text-gray-600" />
           </Link>
           <Link href={"/cart"}>
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M8.25 20.25C8.66421 20.25 9 19.9142 9 19.5C9 19.0858 8.66421 18.75 8.25 18.75C7.83579 18.75 7.5 19.0858 7.5 19.5C7.5 19.9142 7.83579 20.25 8.25 20.25Z"
-                stroke="black"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M18.75 20.25C19.1642 20.25 19.5 19.9142 19.5 19.5C19.5 19.0858 19.1642 18.75 18.75 18.75C18.3358 18.75 18 19.0858 18 19.5C18 19.9142 18.3358 20.25 18.75 20.25Z"
-                stroke="black"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M2.25 3.75H5.25L7.5 16.5H19.5"
-                stroke="black"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M7.5 12.5H19.1925C19.2792 12.5001 19.3633 12.4701 19.4304 12.4151C19.4975 12.3601 19.5434 12.2836 19.5605 12.1986L20.9105 5.44859C20.9214 5.39417 20.92 5.338 20.9066 5.28414C20.8931 5.23029 20.8679 5.18009 20.8327 5.13717C20.7975 5.09426 20.7532 5.05969 20.703 5.03597C20.6528 5.01225 20.598 4.99996 20.5425 5H6"
-                stroke="black"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <IoCartOutline size={24} className="text-gray-600" />
           </Link>
+
+          <button className="p-1 rounded-md ring-1 ring-gray-400 md:hidden">
+            <IoMenuOutline size={24} className="text-gray-600" />
+          </button>
         </div>
       </div>
     </div>
--- app/Components/topBar.tsx
+++ app/Components/topBar.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import { IoChevronDownOutline } from "react-icons/io5";
 
 export const TopBarComponent = () => {
   return (
     <div className="bg-slate-900 min-w-full flex items-center p-4">
       <div className="container flex items-center justify-center space-x-20 mx-auto">
         <div className="flex space-x-4">
-          <p className="text-gray-300 text-center text-sm md:text-base lg:text-md">
+          <p className="text-gray-300 text-center text-xs md:text-base lg:text-md">
             Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
           </p>
           <Link
@@ -16,21 +17,10 @@ export const TopBarComponent = () => {
           </Link>
         </div>
         <div className="hidden items-center justify-center space-x-2 md:flex">
-          <button className="text-gray-300 font-bold text-sm md:text-lg lg:text-xl">
+          <button className="text-gray-300 text-sm md:text-md lg:text-lg">
             English
           </button>
-          <svg
-            width="10"
-            height="8"
-            viewBox="0 0 13 8"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M6.36403 4.95L11.314 0L12.728 1.414L6.36403 7.778L2.67029e-05 1.414L1.41403 0L6.36403 4.95Z"
-              fill="#fff"
-            />
-          </svg>
+          <IoChevronDownOutline size={20} className="text-gray-400" />
         </div>
       </div>
     </div>
