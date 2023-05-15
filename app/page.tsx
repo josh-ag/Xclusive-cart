4c492cc280bfeb7515c6134fd6a798df664beade
joshag <tech.gems.biz@gmail.com>
2023-05-15 22:47:47 +0100

    important update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
 // "use client";
 
+//@imports
 import Image from "next/image";
-// import { Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import Link from "next/link";
-
 import showcaseImage from "../assets/images/iphone.png";
 import appleLogo from "../assets/icons/apple_logo.svg";
 import arrowRight from "../assets/icons/arrow-right.svg";
+import ak_900_wired_keyboard from "../assets/images/ak_900_wired_keyboard.png";
+import havit_hv_g92_gamepad from "../assets/images/havit_hv_g92_gamepad.png";
+import ips_lcd_gaming_monitor from "../assets/images/ips_lcd_gaming_monitor.png";
+import s_series_confort_chair from "../assets/images/s_series_comfort_chair.png";
 import { IoArrowBackOutline, IoArrowForwardOutline } from "react-icons/io5";
+
+type ItemType = {
+  ID: number;
+  name: string;
+  amount: number;
+  prevAmount: number;
+  image: any;
+  rating: number;
+};
 
 const sideNav = [
   { name: "Women's Fashion", path: "/fashion/women" },
@@ -21,13 +34,48 @@ const sideNav = [
   { name: "Health & Beauty", path: "/health" },
 ];
 
+const shoppingList = [
+  {
+    name: "HAVIT HV-G92 Gamepad",
+    amount: 120,
+    prevAmount: 160,
+    rating: 5,
+    image: havit_hv_g92_gamepad,
+    ID: 1,
+  },
+  {
+    name: "AK-900 Wired Keyboard",
+    amount: 960,
+    prevAmount: 1160,
+    rating: 4,
+    image: ak_900_wired_keyboard,
+    ID: 2,
+  },
+  {
+    name: "IPS LCD Gaming Monitor",
+    amount: 370,
+    prevAmount: 400,
+    rating: 5.5,
+    image: ips_lcd_gaming_monitor,
+    ID: 3,
+  },
+  {
+    name: "S-Series Comfort Chair",
+    amount: 375,
+    prevAmount: 400,
+    rating: 5.5,
+    image: s_series_confort_chair,
+    ID: 4,
+  },
+];
+
 export default function Home() {
   return (
-    <div className="min-w-full space-y-8">
+    <div className="min-w-full space-y-24">
       {/* Showcase  */}
-      <div className="container md:space-x-4 flex justify-between items-stretch mx-auto mb-1">
-        <div className="hidden flex-col justify-starrt space-y-2 border-r border-slate-300 p-4 md:flex">
-          <div className="flex self-start flex-col">
+      <div className="container  md:space-x-4 flex justify-between items-stretch mx-auto mb-1">
+        <div className="hidden basis-1/5 flex-col justify-starrt space-y-2 border-r border-slate-300 p-4 md:flex">
+          <div className="flex self-center flex-col">
             {sideNav.map((item, index) => (
               <Link
                 href={item.path}
@@ -77,61 +125,104 @@ export default function Home() {
       </div>
 
       {/* Today Sale */}
-      <div className="container flex flex-col justify-center space-y-4  mx-auto mb-1">
+      <div className="container flex flex-col justify-center space-y-4  mx-auto">
         <div className="ml-4">
           {/* Bullet  */}
           <div className="flex items-center space-x-2">
             <div className="h-7 w-3 bg-red-600 rounded-sm" />
-            <h4 className="text-red-600 text-base md:text-lg">Today's</h4>
+            <h4 className="text-red-600 text-base font-medium md:text-lg">
+              Today's
+            </h4>
           </div>
         </div>
 
         {/* Heading  */}
         <div className="flex px-4 justify-between items-center space-x-6">
-          <h2 className="text-gray-700 text-base font-medium md:text-2xl lg:text-4xl">
+          <h2 className="text-gray-700 text-lg font-bold md:text-2xl lg:text-4xl">
             Flash Sales
           </h2>
           <div className="flex items-center justify-between space-x-2">
-            <div className="flex flex-col items-center">
-              <p className="text-gray-700 text-xs md:text-md">Days</p>
-              <h2 className="text-gray-700 text-base font-medium md:text-2xl lg:text-4xl">
+            <div className="flex flex-col items-center justify-center space-y-2">
+              <p className="text-gray-700 font-medium text-xs md:text-md">
+                Days
+              </p>
+              <h2 className="text-gray-700 text-xl font-bold md:text-2xl lg:text-4xl">
                 03
               </h2>
             </div>
             <span className="text-2xl text-red-700">:</span>
 
-            <div className="flex flex-col items-center">
-              <p className="text-gray-700 text-xs font-normal">Hours</p>
-              <h2 className="text-gray-700 text-base font-medium md:text-2xl lg:text-4xl">
+            <div className="flex flex-col space-y-2 items-center justify-center">
+              <p className="text-gray-700 font-medium text-xs md:text-md">
+                Hours
+              </p>
+              <h2 className="text-gray-700 text-xl font-bold md:text-2xl lg:text-4xl">
                 23
               </h2>
             </div>
             <span className="text-2xl text-red-700">:</span>
 
-            <div className="flex flex-col items-center">
-              <p className="text-gray-700 text-xs font-normal">Minutes</p>
-              <h2 className="text-gray-700 text-base font-medium md:text-2xl lg:text-4xl">
+            <div className="flex flex-col space-y-2 items-center justify-center">
+              <p className="text-gray-700 font-medium text-xs md:text-md">
+                Minutes
+              </p>
+              <h2 className="text-gray-700 text-xl font-bold md:text-2xl lg:text-4xl">
                 19
               </h2>
             </div>
             <span className="text-2xl text-red-700">:</span>
 
-            <div className="flex flex-col items-center">
-              <p className="text-gray-700 text-sm font-normal">Seconds</p>
-              <h2 className="text-gray-700 text-base font-medium md:text-2xl lg:text-4xl">
+            <div className="flex flex-col space-y-2 items-center justify-center">
+              <p className="text-gray-700 font-medium text-xs md:text-md">
+                Seconds
+              </p>
+              <h2 className="text-gray-700 text-xl font-bold md:text-2xl lg:text-4xl">
                 56
               </h2>
             </div>
           </div>
 
-          <div className="flex basis-2/4 items-center justify-end">
+          <div className="flex basis-2/5 items-center justify-end">
             <button className="bg-gray-200 rounded-lg mr-2 p-1">
-              <IoArrowBackOutline size={20} className="text-gray-500" />
+              <IoArrowBackOutline
+                size={20}
+                className="text-gray-500 w-3 h-3 md:w-5 md:h-5"
+              />
             </button>
             <button className="bg-gray-200 rounded-lg p-1">
-              <IoArrowForwardOutline size={20} className="text-gray-500" />
+              <IoArrowForwardOutline className="text-gray-500 w-3 h-3 md:w-5 md:h-5" />
             </button>
           </div>
+        </div>
+
+        {/* items */}
+        <div
+          className="flex space-x-6 items-center mb-6"
+          style={{ overflowX: "scroll" }}
+        >
+          {shoppingList.map((item: ItemType) => (
+            <div key={item.ID} className="flex flex-col space-y-4 ml-4">
+              <div className="max-w-60 max-h-64 w-60 h-64 bg-gray-100 rounded-sm flex items-center justify-center">
+                <Image
+                  src={item.image}
+                  alt={item.name}
+                  style={{ width: "50%" }}
+                />
+              </div>
+              <div className="space-y-2 mb-4">
+                <h4 className="text-gray-700 text-md font-medium">
+                  {item.name}
+                </h4>
+
+                <h4 className="space-x-3 font-medium">
+                  <span className="text-red-500">${item.amount}</span>
+                  <span className="text-gray-400 line-through">
+                    {item.prevAmount}
+                  </span>
+                </h4>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
